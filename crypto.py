463868c973aa41aeddf82da494ef3a659a2655a0
Jaroslaw Polowy <jarekpolowy@gmail.com>
2022-09-30 06:26:26 +0100

    get market data for specific coin

--- crypto.py
+++ crypto.py
@@ -1,6 +1,9 @@
+from locale import currency
+from urllib import request
 import requests
 
 coinsList = None
+currency = 'EUR'
 
 def getCryptoInfo():
     # [ {'id': '01coin', 'symbol': 'zoc', 'name': '01coin', 'platforms': {}} ]
@@ -9,7 +12,6 @@ def getCryptoInfo():
     if response.ok == True:
         data = response.json()
         print('Server OK')
-        print(data[0])
         print('Number of coins: ',str(len(data)))
         coinsList = data
 
@@ -23,6 +25,15 @@ def findCoinBySymbol(symbol):
     else:
         return None
 
-foundCoin = findCoinBySymbol('eth')
+def getCoinMarketData(coinId):
+    response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids='+coinId+'&vs_currencies='+currency+'&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true')
+    if response.ok:
+        print('Server OK')
+        data = response.json()
+        return data
+    else:
+        return None
+foundCoin = findCoinBySymbol('btc')
 print(foundCoin)
-
+marketData = getCoinMarketData(foundCoin['id'])
+print('Market data: ',marketData)
